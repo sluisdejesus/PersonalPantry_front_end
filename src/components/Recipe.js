763b80664069340b5e9c233f03af66d7d13d9afb
Sara <sluisdejesus@gmail.com>
2021-09-21 16:11:04 +0100

    recipe instructions now displaying as a list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,19 +2,29 @@ import React from "react";
 
 const Recipe = ({chosenRecipe}) => {
 
+  
+    
+    
     if(chosenRecipe) {
 
+   const instructionsList = chosenRecipe.instructions.map((instruction) => {
+        return <li>{instruction}</li>   
+})
     return (
         <ul>
             <li>{chosenRecipe.name}</li>
-            <li>Instructions - {chosenRecipe.instructions}</li>
+            
+            <li>
+            Instructions
+                <ul>
+                    {instructionsList}
+                </ul>
+            </li>
             <li>Ready in - {chosenRecipe.readyInMinutes} minutes</li>
         </ul>
     )
-    
-    } else {
-        return null
-    }
+    } else{
+        return null}
 
 }
 
