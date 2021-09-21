816fef153356cafbe61e692fc98c027036c7edce
Connor <connorjrobertson@hotmail.co.uk>
2021-09-21 15:48:19 +0100

    conditional rendering for individual components achieved

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from "react";
-import AllRecipes from "../components/AllRecipes";
-import ShoppingList from "../components/ShoppingList";
+// import AllRecipes from "../components/AllRecipes";
+// import ShoppingList from "../components/ShoppingList";
 import PersonalPantryServices from "../services/PersonalPantryServices";
-import Recipe from "../components/Recipe";
+// import Recipe from "../components/Recipe";
+import RecipeContainer from "./RecipeContainer"
+import ShoppingListContainer from "./ShoppingListContainer"
 
 const AppContainer = () => {
 
     const [allRecipes, setAllRecipes] = useState([])
     const [shoppingList, setShoppingList] = useState({})
     const [ingredients, setIngredients] = useState([])
-    const [chosenRecipe, setChosenRecipe] = useState(null)
 
+    const [shoppingListClick, setShoppingListClick] = useState(false)
+    const [recipesListClick, setRecipesListClick] = useState(false)
+    
     useEffect(() => {
         PersonalPantryServices.getRecipes()
             .then(allRecipes => setAllRecipes(allRecipes));
@@ -24,20 +28,35 @@ const AppContainer = () => {
             .then(shoppingList => setShoppingList(shoppingList));
     }, [])
 
-    const onRecipeChosen = (recipe) => {
-        setChosenRecipe(recipe)
+
+    const handleAllRecipesClick = event => {
+        setRecipesListClick(true)
     }
 
-    // useEffect(() => {
 
-    // }, [chosenRecipe])
+    const handleShoppingListClick = event => {
+        setShoppingListClick(true)
+    }
+
+
+    if(recipesListClick === true) {
+        return(
+            <RecipeContainer allRecipes={allRecipes}/>
+        )
+    }
+
+
+    if(shoppingListClick === true) {
+        return(
+            <ShoppingListContainer shoppingList={shoppingList}/>
+        )
+    }
+
 
     return(
         <>
-        <h1>This is the App Container</h1>
-        <AllRecipes allRecipes={allRecipes} onRecipeChosen={onRecipeChosen} chosenRecipe={chosenRecipe}/>
-        <Recipe chosenRecipe={chosenRecipe}/>
-        <ShoppingList shoppingList={shoppingList}/>
+        <button onClick={handleAllRecipesClick}>View All Recipes</button>
+        <button onClick={handleShoppingListClick}>View Shopping List</button>
         </>
     );
 }
---
+++ src/containers/ShoppingListContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShoppingListContainer = ({shoppingList}) => {
+
+    return(
+        <>
+        <h3>I am the shoppingList</h3>
+        </>
+    )
+}
+
+export default ShoppingListContainer;
