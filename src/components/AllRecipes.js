913b3d29c35e00faf423b5a3e81e603fc9ddf4df
Connor <connorjrobertson@hotmail.co.uk>
2021-09-21 22:50:03 +0100

    improvements to recipe.js - now showing RecipeIngredients

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-const AllRecipes = ({allRecipes, onRecipeChosen, chosenRecipe}) => {
+const AllRecipes = ({allRecipes, onRecipeChosen}) => {
 
     const handleClick = (event) => {
         
        const chosenRecipe = allRecipes[event.target.value]
        onRecipeChosen(chosenRecipe)
-        
     }
 
     const recipeNodes = allRecipes.map((recipe, index) => {
         return(
-            <section key={recipe.id} onClick={handleClick} value={index}>
-            
+            <section key={recipe.id} value={index} onClick={handleClick}>
                 <ul>
-                    <li><img src={recipe.imageUrl} alt={recipe.name}/></li>
+                    <img src={recipe.imageUrl} alt={recipe.name}/>
                     <li>{recipe.name}</li>
                     <li>Ready in {recipe.readyInMinutes} minutes</li>
                     <li>Calories {recipe.caloriesPerServing} kcal</li>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,6 +5,14 @@ const Recipe = ({chosenRecipe}) => {
    const instructionsList = chosenRecipe.instructions.map((instruction) => {
         return <li key={chosenRecipe.id}>{instruction}</li>   
     })
+
+    const recipeIngredientsList = chosenRecipe.recipeIngredients.map((recipeIngredient) => {
+        return (
+            <ul key={recipeIngredient.id}>
+                <li>{recipeIngredient.measure}{(recipeIngredient.ingredient.unitType).toLowerCase()} {recipeIngredient.ingredient.name}</li>
+            </ul>
+        )
+    })
     
     return (
         <section>
@@ -12,6 +20,9 @@ const Recipe = ({chosenRecipe}) => {
         <ul>
             <li>{chosenRecipe.name}</li>
             
+            Ingredients
+                {recipeIngredientsList}
+
             <li>
             Instructions
                 <ul>
