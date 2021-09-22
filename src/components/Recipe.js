30cb18d63fcf6427b3b03f225f0e2fa88e4de210
Sara <sluisdejesus@gmail.com>
2021-09-22 10:26:42 +0100

    added input and addToShopping button

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -16,6 +16,9 @@ const Recipe = ({chosenRecipe}) => {
     
     return (
         <section>
+        <label htmlFor="name">Serving size:</label>
+        <input type="number" id="serving_size" min="1"></input>
+        <button>Add to Shopping list</button>
         <img src={chosenRecipe.imageUrl} alt={chosenRecipe.name}/>
         <ul>
             <li>{chosenRecipe.name}</li>
